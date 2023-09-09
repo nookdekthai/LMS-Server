6cf18ac734f20587f621fb7ed5c855f8523fa9d6
21 Tech Enterprise <21techenterprise@Macintosh.local>
2023-09-09 19:29:15 +0700

    done

--- app.ts
+++ app.ts
@@ -21,7 +21,7 @@ app.use(cookieParser());
 // cors => cross origin resource sharing
 app.use(
   cors({
-    origin: ["https://e-learning-client-nine.vercel.app"],
+    origin: ["https://lms-learning-client.vercel.app"],
     credentials: true,
   })
 );
