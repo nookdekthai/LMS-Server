edc04492a4e299da37ecdec089a0b43e23093388
21 Tech Enterprise <21techenterprise@Macintosh.local>
2023-09-09 19:43:48 +0700

    final

--- build/app.js
+++ build/app.js
@@ -23,7 +23,7 @@ exports.app.use(express_1.default.json({ limit: "50mb" }));
 exports.app.use((0, cookie_parser_1.default)());
 // cors => cross origin resource sharing
 exports.app.use((0, cors_1.default)({
-    origin: ["https://e-learning-client-nine.vercel.app"],
+    origin: ["https://lms-learning-client.vercel.app"],
     credentials: true,
 }));
 // api requests limit
